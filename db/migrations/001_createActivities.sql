4ec720bcf7c69f2d9b96f06347afd65487f28f2b
Donovan Persent <donovan.persent@noconsulting.fr>
2021-12-23 22:42:50 +0100

    ✨ Add migrations, common activity and repeat activity

--- db/migrations/001_createActivities.sql
+++ db/migrations/001_createActivities.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS activities
 (
     -- Fields
     id           bigint primary key auto_increment,
-    name         varchar(64) not null,
+    name         varchar(64)            not null,
     description  varchar(2048),
     repeatable   bit      default false,
     creationDate datetime default now() not null,
